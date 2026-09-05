1e05b5c26bfd7fd6457f048add2f79e37e084d2e
Chris Chekay <sycholic@gmail.com>
2026-09-05 02:05:01 -0400

    Added difficulty options, fixed dup glitch with Loyalty

--- src/main/java/com/sycholic/tridents4players/PoachTridents.java
+++ src/main/java/com/sycholic/tridents4players/PoachTridents.java
@@ -26,34 +26,88 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
-
 package com.sycholic.tridents4players;
 
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Trident;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.AbstractArrow.PickupStatus;
+import java.util.concurrent.ThreadLocalRandom;
+import org.bukkit.inventory.ItemStack;
 
 /**
  *
  * @author sycholic
- * 
+ *
  */
 public class PoachTridents extends JavaPlugin implements Listener {
-    
+
+    private boolean hardmodeEnabled;
+    private boolean breakableEnabled;
+
     @Override
     public void onEnable() {
+        this.saveDefaultConfig();
+        this.loadPluginConfig();
+        getLogger().info("Plugin enabled and configuration loaded successfully!");
         getServer().getPluginManager().registerEvents(this, this);
-        }
-    
+    }
+
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onTridentLaunch(ProjectileHitEvent e) {
         if (e.getEntity() instanceof Trident x) {
-            x.setShooter(null);
+            if (x.getShooter() instanceof Player player) {
+                return;
+            }
+            if (this.breakableEnabled) {
+                if (ThreadLocalRandom.current().nextBoolean()) {
+                    x.remove();
+                    return;
+                }
+            }
+            if (!this.hardmodeEnabled) {
+                x.setShooter(null);
+            }
             x.setPickupStatus(PickupStatus.ALLOWED);
         }
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onTridentHit(ProjectileHitEvent e) {
+        if (e.getEntity() instanceof Trident x) {
+            if (x.getShooter() instanceof Player player) {
+                ItemStack tridentItem = x.getItem();
+                int loyaltyLevel = tridentItem.getEnchantmentLevel(Enchantment.LOYALTY);
+                if (loyaltyLevel > 0) {
+                    return;
+                }
+            }
+            if (this.breakableEnabled) {
+                if (ThreadLocalRandom.current().nextBoolean()) {
+                    x.remove();
+                }
+            }
+        }
+    }
+
+    public void loadPluginConfig() {
+        this.reloadConfig();
+        FileConfiguration config = this.getConfig();
+        this.hardmodeEnabled = config.getBoolean("hardmode-Enabled", false);
+        this.breakableEnabled = config.getBoolean("breakable-Enabled", false);
+    }
+
+    public boolean isHardmodeEnabled() {
+        return hardmodeEnabled;
+    }
+
+    public boolean isBreakableEnabled() {
+        return breakableEnabled;
     }
 }
